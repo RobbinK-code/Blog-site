3f577edd02c4ec29ad5c123e27be6f2c0244638c
RobbinK-code <robbin.kimani@student.moringaschool.com>
2026-05-20 21:27:55 +0300

    Update: add missing import statement for expect and test from vitest in App.test.jsx

--- src/src/App.test.jsx
+++ src/src/App.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import {expect, test} from 'vitest';
 import '@testing-library/jest-dom';
 import App from "./components/App";
 
