f8baf738c0ef39e63bd2a994d31ef33ea8c56cdb
RobbinK-code <robbin.kimani@student.moringaschool.com>
2026-05-20 21:30:22 +0300

    Update: replace jest-dom import with react import in App.test.jsx

--- src/src/App.test.jsx
+++ src/src/App.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import {expect, test} from 'vitest';
-import '@testing-library/jest-dom';
+import '@testing-library/react';
 import App from "./components/App";
 
 test("renders blog title", () => {
