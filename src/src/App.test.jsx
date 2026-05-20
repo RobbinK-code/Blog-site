88f0cc99daeaf57b67c9bd36db5d5ff94d694738
RobbinK-code <robbin.kimani@student.moringaschool.com>
2026-05-20 18:01:08 +0300

    Update: change import path for jest-dom in App.test.jsx

--- src/src/App.test.jsx
+++ src/src/App.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { test, expect } from "vitest";
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/vitest"; 
 
 import App from "./components/App";
 
