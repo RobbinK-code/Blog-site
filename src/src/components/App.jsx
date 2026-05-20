7b1eea4725b0a789de18c43adf280281ad707627
RobbinK-code <robbin.kimani@student.moringaschool.com>
2026-05-20 15:52:08 +0300

    Update: Refactor App component and update tests

--- src/src/components/App.jsx
+++ src/src/components/App.jsx
@@ -2,11 +2,11 @@
 // Parent component for the blog application
 // Connected to: Header, About, ArticleList
 
-import Header from "./components/Header";
-import About from "./components/About";
-import ArticleList from "./components/ArticleList";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 
-import blog from "./data/blog";
+import blog from "../data/blog";
 
 function App() {
   return (
