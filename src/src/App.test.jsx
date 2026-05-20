316d9fe0b59ee99184a9c2ff3acd9ce797ecdebb
RobbinK-code <robbin.kimani@student.moringaschool.com>
2026-05-20 21:21:59 +0300

    refactor: update test setup and improve test descriptions in App.test.jsx

--- src/src/App.test.jsx
+++ src/src/App.test.jsx
@@ -1,12 +1,9 @@
-import React from "react";
 import { render, screen } from '@testing-library/react';
-import { test, expect } from "vitest";
-import "@testing-library/jest-dom/vitest"; 
-
+import '@testing-library/jest-dom';
 import App from "./components/App";
-test("renders the blog title", () => {
-  render(<App />);
 
+test("renders blog title", () => {
+  render(<App />);
   const titleElement = screen.getByText(/Overreacted/i);
   expect(titleElement).toBeInTheDocument();
 });
