940b21f55531d7e96e0221eb7c5510f60076ce8b
RobbinK-code <robbin.kimani@student.moringaschool.com>
2026-05-20 18:12:31 +0300

    Update: remove duplicate import of blogData in App component

--- src/src/components/App.jsx
+++ src/src/components/App.jsx
@@ -4,13 +4,10 @@ import Header from "./Header";
 import About from "./About";
 import ArticleList from "./ArticleList";
 
-<<<<<<< HEAD:src/src/components/App.jsx
 import blog from "../data/blog";
-=======
 import blogData from "../data/blog";
 
 console.log(blogData);
->>>>>>> 42a7d51de7727cfa7320a520d8212d81cf360d05:src/src/App.jsx
 
 function App() {
   return (
