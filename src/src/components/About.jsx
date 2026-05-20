5b16b77f047fc5e0b80e6d017d0aff5a4f237d4b
Robbin Kimani <robbin.kimani@student.moringaschool.com>
2026-05-20 15:06:21 +0300

    Update About component to include default image
    Refactor About component to use default image prop.

--- src/src/components/About.jsx
+++ src/src/components/About.jsx
@@ -1,11 +1,12 @@
-// About component
-// Displays information about the blog
-// Connected to: App.jsx
+function About({
+  image = "https://via.placeholder.com/215",
+  about,
+}) {
+  // This component displays information about the blog
 
-function About({ image, about }) {
   return (
     <aside>
-      <img src={image} alt="blog logo" width="250" />
+      <img src={image} alt="blog logo" />
 
       <p>{about}</p>
     </aside>
